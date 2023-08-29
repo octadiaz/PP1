c63c1906e3298789040407ac3fa599cfde15cef5
Gabi <gabriel.ar.99@hotmail.com>
2023-08-29 18:27:13 -0300

    3.0

---
+++ carrito/src/DataFixtures/ProductoFixtures.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use App\Entity\Producto;
+
+class ProductoFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+ {
+ // creamos 10 productos
+ for ($i = 0; $i < 10; $i++) {
+    $producto = new Producto();
+    $producto->setNombre('product '.$i);
+    $producto->setPrecio(mt_rand(10, 100));
+    $producto->setDescripcion('a');
+    $producto->setImagen('b');
+    $manager->persist($producto);
+    }
+    $manager->flush();
+}
+}
